05d8d454bfd85de67991f4643377add74b3132e0
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-01 16:37:01 +0000

    style: center 'about me' img on small screens

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div id="about" className="container grid sm:grid-cols-1 lg:grid-cols-2 w-4/5 mx-auto">
             <div className="my-10">
                 <img
-                    className="rounded w-96 h-96"
+                    className="mx-auto lg:mx-0 rounded w-96 h-96"
                     src="https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="about"></img>
             </div>
             <div className="mx-auto my-10">
