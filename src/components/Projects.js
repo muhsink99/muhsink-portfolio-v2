be98ba49bcacc75dbb7560090dcf09c1c79b4d11
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-01 16:22:15 +0000

    feat: add all projects to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import * as React from "react"
-import { DiSqllite, DiHtml5, DiDjango, DiCss3, DiBootstrap } from "react-icons/di";
-import { FaLink, FaGithub } from "react-icons/fa";
+import {
+    DiSqllite, DiHtml5, DiDjango, DiCss3, DiBootstrap, DiReact, DiFirebase, DiDart,
+    DiSass, DiJavascript, DiPython
+} from "react-icons/di";
+import { FaLink, FaGithub, FaTimesCircle } from "react-icons/fa";
 
 //TODO: add images for projects 
 
@@ -12,11 +15,12 @@ const Projects = () => {
                     projects
                 </h1>
                 <div className="container grid lg:grid-cols-2 md:grid-cols-1 gap-8 mx-auto mb-5">
-                    <div className="my-auto">
+                    <div className="my-auto mr-2">
                         <h2 className="font-bold text-blue-400 text-2xl mb-2">Backlog Tracker</h2>
                         <div className="flex mb-3">
                             <div className="technology-card"><DiSqllite className="icon-standard" /><span className="text-xs">SQLlite</span></div>
                             <div className="technology-card"><DiHtml5 className="icon-standard" /><span className="text-xs">HTML5</span></div>
+                            <div className="technology-card"><DiPython className="icon-standard" /><span className="text-xs">Python</span></div>
                             <div className="technology-card"><DiDjango className="icon-standard" /><span className="text-xs">Django</span></div>
                             <div className="technology-card"><DiCss3 className="icon-standard" /><span className="text-xs">CSS3</span></div>
                             <div className="technology-card"><DiBootstrap className="icon-standard" /><span className="text-xs">Bootstrap</span></div>
@@ -27,7 +31,62 @@ const Projects = () => {
                             <a href="https://github.com/muhsink99/backlog_tracker" className="flex btn-primary mr-3"><FaGithub className="mr-3" />Github</a>
                         </div>
                     </div>
-                    <div className="ml-auto">
+                    <div className="mx-auto lg:ml-auto lg:mx-0">
+                        <img className="rounded" src="https://picsum.photos/400"></img>
+                    </div>
+
+                    <div className="my-auto">
+                        <h2 className="font-bold text-blue-400 text-2xl mb-2">Recipe Book</h2>
+                        <div className="flex mb-3">
+                            <div className="technology-card"><DiReact className="icon-standard" /><span className="text-xs">React</span></div>
+                            <div className="technology-card"><DiHtml5 className="icon-standard" /><span className="text-xs">HTML5</span></div>
+                            <div className="technology-card"><DiCss3 className="icon-standard" /><span className="text-xs">CSS3</span></div>
+                            <div className="technology-card"><DiSass className="icon-standard" /><span className="text-xs">Sass</span></div>
+                            <div className="technology-card"><DiBootstrap className="icon-standard" /><span className="text-xs">Bootstrap</span></div>
+                            <div className="technology-card"><DiFirebase className="icon-standard" /><span className="text-xs">Firebase</span></div>
+                            <div className="technology-card"><DiJavascript className="icon-standard" /><span className="text-xs">Javascript</span></div>
+                            <div className="technology-card"><DiJavascript className="icon-standard" /><span className="text-xs">Javascript</span></div>
+                        </div>
+                        <p className="text-lg mb-3">Allows its user to compile a list of recipes in one contained place. </p>
+                        <div id="project-buttons" className="flex">
+                            <a href="https://github.com/muhsink99/recipe-book" className="flex btn-primary mr-3"><FaGithub className="mr-3" />Github</a>
+                        </div>
+                    </div>
+                    <div className="mx-auto lg:ml-auto lg:mx-0">
+                        <img className="rounded" src="https://picsum.photos/400"></img>
+                    </div>
+
+                    <div className="my-auto">
+                        <h2 className="font-bold text-blue-400 text-2xl mb-2">Remote Healthcare Tracker</h2>
+                        <div className="flex mb-3">
+                            <div className="technology-card"><DiDart className="icon-standard" /><span className="text-xs">Dart</span></div>
+                            <div className="technology-card"><DiFirebase className="icon-standard" /><span className="text-xs">Firebase</span></div>
+                        </div>
+                        <p className="text-lg mb-3">A cross-platform healthcare app to allow clinicians to keep track of opt-in patients for vital sign recordings + more. Made with the goal to help reduce cardivascular disease risk </p>
+                        <div id="project-buttons" className="flex">
+                            <span href="https://github.com/muhsink99/backlog_tracker" className="flex btn-gray mr-3"><FaTimesCircle className="mr-3" />Closed Source</span>
+                            <a href="https://github.com/muhsink99/backlog_tracker" className="flex btn-primary mr-3"><FaLink className="mr-3" />Video Presentation</a>
+                        </div>
+                    </div>
+                    <div className="mx-auto lg:ml-auto lg:mx-0">
+                        <img className="rounded" src="https://picsum.photos/400"></img>
+                    </div>
+
+                    <div className="my-auto">
+                        <h2 className="font-bold text-blue-400 text-2xl mb-2">Portfolio Website</h2>
+                        <div className="flex mb-3">
+                            <div className="technology-card"><DiHtml5 className="icon-standard" /><span className="text-xs">HTML5</span></div>
+                            <div className="technology-card"><DiCss3 className="icon-standard" /><span className="text-xs">CSS3</span></div>
+                            <div className="technology-card"><DiSass className="icon-standard" /><span className="text-xs">Sass</span></div>
+                            <div className="technology-card"><DiReact className="icon-standard" /><span className="text-xs">React</span></div>
+                            <div className="technology-card"><DiJavascript className="icon-standard" /><span className="text-xs">Javascript</span></div>
+                        </div>
+                        <p className="text-lg mb-3">A website to show various my various projects and skills</p>
+                        <div id="project-buttons" className="flex">
+                            <a href="https://github.com/muhsink99/muhsink-portfolio-v2" className="flex btn-primary mr-3"><FaGithub className="mr-3" />Github</a>
+                        </div>
+                    </div>
+                    <div className="mx-auto lg:ml-auto lg:mx-0">
                         <img className="rounded" src="https://picsum.photos/400"></img>
                     </div>
                 </div>
