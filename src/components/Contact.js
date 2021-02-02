97d3ff1fd35f71f93400d6dba6e8374a4a30a103
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-02 02:00:35 +0000

    feat: add contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,17 @@
 import * as React from "react"
-import { Link } from 'gatsby';
-import { FaLinkedin } from "react-icons/fa";
-
-import { initials } from '../../config';
+import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Contact = () => {
     return (
         <div id="contact" className="container mx-auto w-4/5">
-            <div className="my-10">
-                <section>
-                    <h1 className="uppercase font-bold text-4xl mb-3">
-                        Contact
-                    </h1>
-                    <div className="text-xl flex flex-col mx-auto">
-                        <p className="text-left lg:text-center mb-5">Interested? Feel free to contact me via the following:</p>
-                        <a href="linkedin.com/muhsin-kumbay" class="btn-primary flex-shrink min-w-64 max-w-128 text-center mb-3">LinkedIn</a>
-                        <a href="linkedin.com/muhsin-kumbay" class="btn-primary flex-shrink min-w-64 max-w-128 text-center mb-3">E-Mail (kumbaymuhsin@gmail.com)</a>
-                    </div>
-                </section>
-            </div>
+            <section className="my-10">
+                <h1 className="uppercase font-bold text-4xl mb-3">
+                    Contact
+                </h1>
+                <p className="text-left lg:text-center mb-5">Interested? Feel free to contact me via the following:</p>
+                <a href="https://www.linkedin.com/in/muhsin-kumbay/" class="flex justify-center btn-primary text-center mb-3"><FaLinkedin className="mr-3 my-auto" />LinkedIn</a>
+                <a href="mailto:kumbaymuhsin@gmail.com" class="flex justify-center btn-primary text-center mb-3"><FaEnvelope className="mr-3 my-auto" />E-Mail (kumbaymuhsin@gmail.com)</a>
+            </section>
         </div>
     );
 }
