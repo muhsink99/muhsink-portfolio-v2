39ff3bf2c5802a2fd1629a9871f14344e315c155
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-02 02:05:04 +0000

    style: adjust icon button placement

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,8 +27,8 @@ const Projects = () => {
                         </div>
                         <p className="text-lg mb-3">Built to help its user keep track of various shows from various streaming services</p>
                         <div id="project-buttons" className="flex justify-center">
-                            <a href="https://muhsink99.pythonanywhere.com/" className="flex btn-primary mr-3"><FaLink className="mr-3" />Live Link</a>
-                            <a href="https://github.com/muhsink99/backlog_tracker" className="flex btn-primary mr-3"><FaGithub className="mr-3" />Github</a>
+                            <a href="https://muhsink99.pythonanywhere.com/" className="flex btn-primary mr-3"><FaLink className="btn-icon" />Live Link</a>
+                            <a href="https://github.com/muhsink99/backlog_tracker" className="flex btn-primary mr-3"><FaGithub className="btn-icon" />Github</a>
                         </div>
                         <div className="mt-5">
                             <img className="rounded mx-auto w-400 h-400" src={require("/src/images/Projects/project-1-1.png")}></img>
@@ -49,10 +49,10 @@ const Projects = () => {
                         </div>
                         <p className="text-lg mb-3">Allows its user to compile a list of recipes in one contained place. </p>
                         <div id="project-buttons" className="flex justify-center">
-                            <a href="https://github.com/muhsink99/recipe-book" className="flex btn-primary mr-3"><FaGithub className="mr-3" />Github</a>
+                            <a href="https://github.com/muhsink99/recipe-book" className="flex btn-primary mr-3"><FaGithub className="btn-icon" />Github</a>
                         </div>
                         <div className="mt-5">
-                            <img className="rounded mx-auto" src={require("/src/images/Projects/project-2-1.png")}></img>
+                            <img className="rounded mx-auto" src={require("/src/images/Projects/project-1-1.png")}></img>
                         </div>
                     </div>
 
@@ -64,7 +64,8 @@ const Projects = () => {
                         </div>
                         <p className="text-lg mb-3">A cross-platform healthcare app to allow clinicians to keep track of opt-in patients for vital sign recordings + more. Made with the goal to help reduce cardivascular disease risk </p>
                         <div id="project-buttons" className="flex justify-center">
-                            <span className="flex btn-gray mr-3"><FaTimesCircle className="mr-3" />Closed Source</span>                        </div>
+                            <span className="flex btn-gray mr-3"><FaTimesCircle className="btn-icon" />Closed Source</span>
+                        </div>
                         <div className="mt-5">
                             <img className="rounded mx-auto w-64 h-128" src={require("/src/images/Projects/project-3-1.png")} ></img>
                         </div>
@@ -81,7 +82,7 @@ const Projects = () => {
                         </div>
                         <p className="text-lg mb-3">A website to show my various projects and skills</p>
                         <div id="project-buttons" className="flex justify-center">
-                            <a href="https://github.com/muhsink99/muhsink-portfolio-v2" className="flex btn-primary mr-3"><FaGithub className="mr-3" />Github</a>
+                            <a href="https://github.com/muhsink99/muhsink-portfolio-v2" className="flex btn-primary mr-3"><FaGithub className="btn-icon" />Github</a>
                         </div>
                         <div className="my-auto">
                             <img className="rounded mx-auto w-400 h-400" src={require("/src/images/Projects/project-4-1.png")} ></img>
