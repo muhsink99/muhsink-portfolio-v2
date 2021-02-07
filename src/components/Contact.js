a65138664c6045bd93e441bf0d885000677b1947
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-07 12:31:31 +0000

    feat: add e-mail contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 
+//                 <a href="mailto:kumbaymuhsin@gmail.com" className="flex justify-center btn-primary text-center mb-3"><FaEnvelope className="btn-icon" />E-Mail (kumbaymuhsin@gmail.com)</a>
 const Contact = () => {
     return (
         <div id="contact" className="container mx-auto w-4/5">
@@ -9,10 +10,24 @@ const Contact = () => {
                     Contact
                 </h1>
                 <p className="text-lg text-center mb-5">Interested? Feel free to contact me via the following:</p>
-                <a href="https://www.linkedin.com/in/muhsin-kumbay/" className="flex justify-center btn-primary text-center mb-3"><FaLinkedin className="btn-icon" />LinkedIn</a>
-                <a href="mailto:kumbaymuhsin@gmail.com" className="flex justify-center btn-primary text-center mb-3"><FaEnvelope className="btn-icon" />E-Mail (kumbaymuhsin@gmail.com)</a>
+                <div className="grid lg:grid-cols-2 sm:grid-cols-1">
+                    <div id="contact-form" className="mx-10">
+                        <h2 className="mb-3">Send an e-mail to me: </h2>
+                        <form className="flex flex-col">
+                            <label className="mb-3">Full name</label><input type="text" aria-label="Full name" className="form-input"></input>
+                            <label className="mb-3">E-Mail</label><input type="text" aria-label="Email" className="form-input"></input>
+                            <label className="mb-3">Message</label><textarea type="text" aria-label="Message" className="form-input"></textarea>
+                            <button className="btn-primary text-center">Send message</button>
+                        </form>
+                    </div>
+                    <div id="socials" className="my-auto mx-10">
+                        <h2 className="my-3">Socials:</h2>
+                        <a href="https://www.linkedin.com/in/muhsin-kumbay/" className="flex justify-center btn-primary text-center mb-3"><FaLinkedin className="btn-icon" />LinkedIn</a>
+                    </div>
+                </div>
+
             </section>
-        </div>
+        </div >
     );
 }
 
