0decb4aa9e9094f5e75eaab616b7824cfa45d7e2
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-07 12:03:59 +0000

    style: add padding to container

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,18 +4,13 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
     return (
-        <>
-            <head>
-                <meta name="viewport" content="width=1060px, maximum-scale=1.0" />
-            </head>
-            <div
-                className="min-h-full max-w-screen py-3 grid"
-                style={{ gridTemplateRows: "auto 1fr auto auto" }}>
-                <Header />
-                <main>{children}</main>
-                <Footer></Footer>
-            </div>
-        </>
+        <div
+            className="min-h-full max-w-screen py-3 px-3 grid"
+            style={{ gridTemplateRows: "auto 1fr auto auto" }}>
+            <Header />
+            <main>{children}</main>
+            <Footer></Footer>
+        </div>
     );
 }
 
