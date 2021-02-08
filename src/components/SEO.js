7110c12a938c9015aae50fa083c5ae69ed44f41b
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-08 16:31:01 +0000

    fix: improve SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -5,8 +5,12 @@ const SEO = () => {
     return (
         <Helmet>
             <title>Muhsin Kumbay</title>
+            <meta charset="utf-8"></meta>
+            <meta name="og:title" property="og:title" content="Muhsin Kumbay - Portfolio" />
             <meta name="description" content="A portfolio website to demonstrate Muhsin's various skills and projects."></meta>
-            <meta property="og:image" content={"/readme-image.png"} />
+            <link rel="canonical" href="muhsinkumbay.dev" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta property="og:image" content="/readme-image.png" />
         </Helmet>
     );
 }
