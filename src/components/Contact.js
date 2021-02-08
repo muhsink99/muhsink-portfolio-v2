28266a2410bdb04587ace15adbe61d150ef1a76a
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-08 17:44:47 +0000

    fix: temporarily remove email form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,14 +11,9 @@ const Contact = () => {
                 </h1>
                 <p className="text-lg text-center mb-5">Interested? Feel free to contact me via the following:</p>
                 <div className="grid lg:grid-cols-2 sm:grid-cols-1">
-                    <div id="contact-form" className="mx-10">
-                        <h2 className="mb-3">Send an e-mail to me: </h2>
-                        <form className="flex flex-col">
-                            <label className="mb-3">Full name</label><input type="text" aria-label="Full name" className="form-input"></input>
-                            <label className="mb-3">E-Mail</label><input type="text" aria-label="Email" className="form-input"></input>
-                            <label className="mb-3">Message</label><textarea type="text" aria-label="Message" rows="5" cols="20" className="form-input"></textarea>
-                            <button className="btn-primary text-center">Send message</button>
-                        </form>
+                    <div id="contact-form" className="mx-10 my-auto">
+                        <h2 className="my-3">Send an e-mail to me: </h2>
+                        <a className="flex btn-primary text-center  justify-center mb-3" type="submit" value="Submit">Send message</a>
                     </div>
                     <div id="socials" className="my-auto mx-10">
                         <h2 className="my-3">Socials:</h2>
