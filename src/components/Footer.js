61260c2f08bf7725aade8a2ee6424d55682a20a4
muhsink99 <muhsinkumbay99@gmail.com>
2021-02-17 17:18:21 +0000

    fix: remove social media footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,10 +35,6 @@ const Footer = () => {
                     <a href="https://icons8.com/icon/53562/database">Icons provided by Icons8</a>,
                     © Muhsin Kumbay, 2021
                  </p>
-                <div id="socials" className="mb-2 flex flex-row my-1">
-                    <a href="https://github.com/muhsink99"><FaGithub className="icon-social" /></a>
-                    <a href="https://www.linkedin.com/in/muhsin-kumbay/"><FaLinkedin className="icon-social" /></a>
-                </div>
             </footer>
         </>
     );
